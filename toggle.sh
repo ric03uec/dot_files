2f572c177500f07837b1a3a12c1f066aada13ad7
Devashish <hello@devashish.me>
2025-07-21 16:03:33 -0700

    updating toggle.sh for kubuntu

--- toggle.sh
+++ toggle.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 #######################################
 # DESCRIPTION: Toggles Touchpad ON/OFF
-# DATE: 15.4.2011
-# AUTHOR: ssnjara
+# DATE: Updated for Kubuntu 24.04
+# AUTHOR: ssnjara (updated)
 #######################################
-# Check if Touchpad is ON and set "var" acording
-var=`synclient | grep TouchpadOff | cut -d\= -f2`
-# if ON turn OFF, else turn ON
-if [ $var -eq 0 ]
-then
-synclient TouchpadOff=1
-notify-send -i dialog-error "Touchpad disabled!"
-else
-synclient TouchpadOFF=0
-notify-send -i dialog-information "Touchpad enabled!"
+
+# Find touchpad device ID
+touchpad_id=$(xinput list | grep -i touchpad | grep -o 'id=[0-9]*' | cut -d= -f2)
+
+if [ -z "$touchpad_id" ]; then
+    notify-send -i dialog-error "Touchpad not found!"
+    exit 1
 fi
-#END
+
+# Check if touchpad is enabled (1) or disabled (0)
+enabled=$(xinput list-props "$touchpad_id" | grep "Device Enabled" | grep -o '[01]$')
+
+if [ "$enabled" -eq 1 ]; then
+    # Touchpad is enabled, disable it
+    xinput disable "$touchpad_id"
+    notify-send -i dialog-error "Touchpad disabled!"
+else
+    # Touchpad is disabled, enable it
+    xinput enable "$touchpad_id"
+    notify-send -i dialog-information "Touchpad enabled!"
+fi
